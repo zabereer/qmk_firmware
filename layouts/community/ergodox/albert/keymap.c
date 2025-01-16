a898192584b02797ec1cd4b82bb5b3a5df5f36bc
Albert Pretorius <pretoalb@gmail.com>
2025-01-16 09:26:32 +0000

    unset LC_ALL

--- layouts/community/ergodox/albert/keymap.c
+++ layouts/community/ergodox/albert/keymap.c
@@ -931,6 +931,8 @@ void matrix_scan_user(void) {
             send_keystrokes(KC_ENT, KC_NO);
             SEND_STRING("export HISTCONTROL=ignorespace");
             send_keystrokes(KC_ENT, KC_NO);
+            SEND_STRING("unset LC_ALL");
+            send_keystrokes(KC_ENT, KC_NO);
         }
         SEQ_TWO_KEYS(KC_A, KC_G) {
             SEND_STRING("alias g='grep --color=auto'");
