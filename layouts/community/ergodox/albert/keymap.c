5f7441d7f013766425f34be2fb847fc057e1f560
Albert Pretorius <pretoalb@gmail.com>
2025-04-10 07:23:37 +0100

    jj bookmark move

--- layouts/community/ergodox/albert/keymap.c
+++ layouts/community/ergodox/albert/keymap.c
@@ -973,6 +973,9 @@ void matrix_scan_user(void) {
             send_keystrokes(NK_DOWN, KC_LSFT, KC_QUOT, NK_UP, KC_LSFT, KC_NO);
             SEND_STRING(" & bookmarks()'::");
         }
+        SEQ_TWO_KEYS(KC_J, KC_M) {
+            SEND_STRING("jj bookmark move --to @- ");
+        }
         SEQ_TWO_KEYS(KC_J, KC_P) {
             SEND_STRING("jj git push --remote origin --bookmark ");
         }
